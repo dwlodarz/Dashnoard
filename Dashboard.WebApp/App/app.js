90450a3df7b04f2642c9ce97df3aa5267ae99bc6
Dominik <dwlodarz@krollontrack.com>
2015-11-20 13:27:50 +0100

    calendar working

--- Dashboard.WebApp/App/app.js
+++ Dashboard.WebApp/App/app.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'mwl.calendar', 'ui.bootstrap', 'ngTouch', 'ngAnimate']);
 
 myApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when(
