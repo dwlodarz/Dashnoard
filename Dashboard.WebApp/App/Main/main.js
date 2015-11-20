d9957be14874d55034ae3d563ae9636e59b34de8
Dominik <dwlodarz@krollontrack.com>
2015-11-20 15:02:17 +0100

    data source switch

--- Dashboard.WebApp/App/Main/main.js
+++ Dashboard.WebApp/App/Main/main.js
@@ -6,31 +6,9 @@ myApp.controller("MainController", ['$scope', '$uibModal', 'moment', 'eventServi
     //These variables MUST be set as a minimum for the calendar to work
     $scope.calendarView = 'month';
     $scope.calendarDay = new Date();
-    $scope.events = [
-      {
-          title: 'An event',
-          type: 'warning',
-          startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
-          endsAt: moment().startOf('week').subtract(2, 'days').add(9, 'hours').toDate(),
-          draggable: true,
-          resizable: true
-      }, {
-          title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
-          type: 'info',
-          startsAt: moment().subtract(1, 'day').toDate(),
-          endsAt: moment().subtract(1, 'day').add(1, 'hours').toDate(),
-          draggable: true,
-          resizable: true
-      }, {
-          title: 'This is a really long event title that occurs on every year',
-          type: 'important',
-          startsAt: moment().startOf('day').add(7, 'hours').toDate(),
-          endsAt: moment().startOf('day').add(19, 'hours').toDate(),
-          recursOn: 'year',
-          draggable: true,
-          resizable: true
-      }
-    ];
+    $scope.events = eventService.GetStoredEvent();
+
+
     function showModal(action, event) {
         $uibModal.open({
             templateUrl: 'modalContent.html',
--- Dashboard.WebApp/App/Services/eventService.js
+++ Dashboard.WebApp/App/Services/eventService.js
@@ -1,6 +1,38 @@
-﻿(function() {
+﻿(function () {
     'use strict';
-    angular.module('app.services',[]).service("eventService",  function() {
-        
-    });
+    angular.module('app.services', [])
+        .service("eventService", ['$q', function ($q) {
+
+            function getStoredEvent() {
+                return [
+                  {
+                      title: 'An event',
+                      type: 'warning',
+                      startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
+                      endsAt: moment().startOf('week').subtract(2, 'days').add(9, 'hours').toDate(),
+                      draggable: true,
+                      resizable: true
+                  }, {
+                      title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
+                      type: 'info',
+                      startsAt: moment().subtract(1, 'day').toDate(),
+                      endsAt: moment().subtract(1, 'day').add(1, 'hours').toDate(),
+                      draggable: true,
+                      resizable: true
+                  }, {
+                      title: 'This is a really long event title that occurs on every year',
+                      type: 'important',
+                      startsAt: moment().startOf('day').add(7, 'hours').toDate(),
+                      endsAt: moment().startOf('day').add(19, 'hours').toDate(),
+                      recursOn: 'year',
+                      draggable: true,
+                      resizable: true
+                  }
+                ];
+            }
+
+            return {
+                GetStoredEvent: getStoredEvent
+            };
+        }]);
 })();
