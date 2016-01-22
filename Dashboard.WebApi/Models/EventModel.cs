8b92542f6bea91cecfc0df40baa8b514451b950e
dwlodarz <dominik.wlodarz@gmail.com>
2016-01-22 13:16:20 +0100

    New webApi added

---
+++ Dashboard.WebApi/Models/EventModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dashboard.WebApi.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MyProperty { get; set; }
+        public DateTime StartsAt { get; set; }
+        public DateTime EndsAt { get; set; }
+    }
+}
